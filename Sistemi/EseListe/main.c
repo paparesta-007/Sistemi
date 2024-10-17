c681f5cd0e5530a93520b1cf6c5a0e0e616700ab
unknown <t.paparesta.3231@vallauri.net>
2024-10-17 11:33:57 +0200

    Iniziato esercizio liste e contaliste

--- Sistemi/EseListe/main.c
+++ Sistemi/EseListe/main.c
@@ -1,8 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <malloc.h>
 
+typedef struct{
+    int x;
+    int *p;
+} nodo;
+
+nodo* NuovoNodo();
+void EliminaNodo();
+int contaNodi(nodo* head);
 int main()
 {
+    nodo *HEAD;
+    HEAD=NuovoNodo(NULL);
+    HEAD->x=12;
+
+    HEAD=NuovoNodo(HEAD);
+    HEAD=NuovoNodo(HEAD);
+    HEAD=NuovoNodo(HEAD);
+    HEAD=NuovoNodo(HEAD);
+    HEAD=NuovoNodo(HEAD);
+    HEAD=NuovoNodo(HEAD);
+    HEAD=NuovoNodo(HEAD);
+    HEAD=NuovoNodo(HEAD);
+    HEAD=NuovoNodo(HEAD);
+
+    printf("Nodi totali: %d\n",contaNodi(HEAD));
     printf("Hello world!\n");
     return 0;
+}
+nodo* NuovoNodo(nodo* next){
+    nodo *n_p = (nodo*)malloc(sizeof(nodo));
+    n_p->p = next;
+    return n_p;
+}
+void EliminaNodo(nodo* n_p){
+    printf("Ho cancellato nodo %d\n",n_p->x);
+    free(n_p);
+}
+int contaNodi(nodo* head){
+    int cont=0;
+    while(head!=NULL){
+        cont++;
+        head=head->p;
+    }
+    return cont;
 }
