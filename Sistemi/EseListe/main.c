8c8174ad95804dca45994159612f6c3c005dd097
unknown <t.paparesta.3231@vallauri.net>
2024-10-23 13:20:45 +0200

    continuazione esercizio liste

--- Sistemi/EseListe/main.c
+++ Sistemi/EseListe/main.c
@@ -2,55 +2,113 @@
 #include <stdlib.h>
 #include <malloc.h>
 
-typedef struct
+typedef struct node
 {
     int x;
-    int *p;
-} nodo;
+	//
+	//void* next; // Indirizzo al nodo successivo
+    struct node* next;
+}nodo;
 
+nodo* Inserisci(void* next);
+nodo* EliminaNodo(nodo* n_p); // cambiare n_p
+int contaNodi(nodo* node);
+void mostraLista(nodo* node);
+void inserisciCoda(nodo* node);
+
+
+/* TBD:
+int controlla_lista_vuota(...)
 nodo* NuovoNodo();
-void EliminaNodo();
-int contaNodi(nodo* head);
-int main()
-{
-    nodo *HEAD;
-    HEAD=NuovoNodo(NULL);
-    HEAD->x=12;
+nodo* inserisciTesta(...)
+nodo* inserisciCoda(...)
+nodo* inserisciPosizione(...,int pos)
+nodo* eliminaNodo(...)
+nodo* eliminaByPos(..., int pos);
+void mostraLista(...);
+nodo* aggiungiByPos(..., int pos);
+nodo* aggiungiOrdinato(nodo *testa);
+*/
 
-    HEAD=NuovoNodo(HEAD);
-    HEAD=NuovoNodo(HEAD);
-    HEAD=NuovoNodo(HEAD);
-    HEAD=NuovoNodo(HEAD);
-    HEAD=NuovoNodo(HEAD);
-    HEAD=NuovoNodo(HEAD);
-    HEAD=NuovoNodo(HEAD);
-    HEAD=NuovoNodo(HEAD);
-    HEAD=NuovoNodo(HEAD);
+int main() {
+    nodo* head=NULL;
+    //head=InserisciTesta(NULL); // Ha senso il nome?
+    //head->x=0;
 
-    printf("Nodi totali: %d\n",contaNodi(HEAD));
-    printf("Hello world!\n");
+    for(int i = 0; i < 10; i++)
+    {
+        head = Inserisci(head);
+        head->x = i;
+    }
+
+    mostraLista(head);
+
+    inserisciCoda(head);
+
+    mostraLista(head);
+
+    //head = EliminaNodo(head);
+
+    printf("Numero nodi: %d\n",contaNodi(head));
     return 0;
 }
-nodo* NuovoNodo(nodo* next)
+
+void inserisciCoda(nodo* node)
 {
-    nodo *n_p = (nodo*)malloc(sizeof(nodo));
-    n_p->p = next;
+    nodo* current;
+
+    while(node!=NULL)
+    {
+        current = node;
+        node = current->next;
+    }
+
+    current->next = Inserisci(node);
+    current->next->x = 100;
+}
+
+void mostraLista(nodo* node)
+{
+    while(node!=NULL)
+    {
+        printf("%i, ",node->x,node);
+        node=node->next;
+    }
+    printf("\n");
+}
+
+nodo* Inserisci(void* attuale) {
+    nodo* n_p;
+
+    n_p=(nodo*)malloc(sizeof(nodo));
+    ///Crea un'allocazione nella memoria per una struct nodo
+
+    n_p->next=attuale;
+    ///Imposta nel puntatore della struct l'indirizzo dell'ultimo nodo
+
     return n_p;
 }
-void EliminaNodo(nodo* n_p)
-{
-    printf("Ho cancellato nodo %d\n",n_p->x);
+
+///Ritorna un puntatore al nodo successivo
+nodo* EliminaNodo(nodo* n_p){
+    nodo* aus = n_p->next;
+
+    printf("Ho cancellato nodo %d \n\n",n_p->x);
+
     free(n_p);
+
+    return aus;
 }
-int contaNodi(nodo* head)
+
+int contaNodi(nodo* node)
 {
-    int cont=0;
-    if(head->p==NULL)
-        return 0;
-    while(head!=NULL)
+    int i=0;
+
+    while(node!=NULL)
     {
-        cont++;
-        head=head->p;
+        node=node->next;
+        i++;
     }
-    return cont;
+
+    return i;
 }
